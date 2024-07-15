9760c1331031e5f65f3bf91400ea019851a3bc42
Daoxin Zhong <daoxin.zhong@hmc.ox.ac.uk>
2024-07-15 21:42:06 +0000

    python script

--- masked_dust3r/scripts/match_images.py
+++ masked_dust3r/scripts/match_images.py
@@ -13,6 +13,7 @@ import numpy as np
 from PIL import Image
 import torch
 import json
+import re
 import open3d as o3d
 
 from dust3r.inference import inference_with_mask, create_gaussian_kernel
@@ -25,42 +26,41 @@ from masked_dust3r.scripts.utils.math import *
 from masked_dust3r.scripts.utils.image import *
 
 
-DATA_PATH = "/dust3r/masked_dust3r/data/jackal_color_wheel_training_data_1"
+DATA_PATH = "/dust3r/masked_dust3r/data/jackal_irl_one_spin"
 IMG_FILE_EXTENSION = ".png"
 MASK_FILE_EXTENSION = ".png"
 
-INIT_FRAMES = 20
-NEW_FRAMES = 5
-PREVIOUS_FRAMES = 10
-TOTAL_FRAMES = 300
+INIT_FRAMES = 50
+NEW_FRAMES = 20
+PREVIOUS_FRAMES = 5
+TOTAL_FRAMES = 140
 
-INIT_WEIGHT_FOCAL = 0.1
+INIT_WEIGHT_FOCAL = 0.01 * 0
 INIT_WEIGHT_Z = 0.1 
-INIT_WEIGHT_ROT = 0.01
-INIT_WEIGHT_TRANS_SMOOTHNESS = 0.0001 
+INIT_WEIGHT_ROT = 0.1 * 0
+INIT_WEIGHT_TRANS_SMOOTHNESS = 0.001 * 0
 INIT_WEIGHT_ROT_SMOOTHNESS = 0.001 * 0
 
-NEW_WEIGHT_FOCAL = 0.1
+NEW_WEIGHT_FOCAL = 0.1 * 0
 NEW_WEIGHT_Z = 0.1
-NEW_WEIGHT_ROT = 0.01
+NEW_WEIGHT_ROT = 0.1
 NEW_WEIGHT_TRANS_SMOOTHNESS = 0.0001
-NEW_WEIGHT_ROT_SMOOTHNESS = 0.00001 * 0
+NEW_WEIGHT_ROT_SMOOTHNESS = 0.00001
 
-USE_COMMON_INTRINSICS = True
-IS_FOCAL_FIXED = True
+USE_COMMON_INTRINSICS = False
 
 device = 'cuda'
 batch_size = 1
 schedule = 'cosine'
 lr = 0.01
-niter = 300 
+niter = 3000
 
-GAUSSIAN_SIGMA = 51.0
+GAUSSIAN_SIGMA = 21.0
 SIZE = int(GAUSSIAN_SIGMA * 3)
 
 kernel = create_gaussian_kernel(SIZE, GAUSSIAN_SIGMA).to(device)
 
-SIZE = 11
+SIZE = 1
 kernel = torch.ones(SIZE, SIZE).to(device)
 
 # Load the model
@@ -80,7 +80,7 @@ images = load_images(images_array, size=512, verbose=True)
 _,_,H,W = images[0]["img"].shape
 masks = load_masks(masks_array, H, W, device)
 
-pairs = make_pairs(images, scene_graph='swin-5', prefilter=None, symmetrize=True)
+pairs = make_pairs(images, scene_graph='swin-3', prefilter=None, symmetrize=True)
 output = inference_with_mask(pairs, model, device, masks, kernel, batch_size=batch_size)
 del pairs
 
@@ -103,7 +103,8 @@ intrinsics = scene.get_intrinsics()
 
 img = Image.open(images_array[0])
 width, height = img.size
-RESCALE_FACTOR  = width/512
+RESCALE_FACTOR = width/512
+FIXED_FOCAL_LENGTH = 4.74
 
 transforms = {}
 transforms["camera_model"] = "OPENCV"
@@ -127,8 +128,11 @@ for i in range(len(poses)):
     if not((confidence_masks[i]==0).all()):
         frame = {}
         frame["file_path"] = "/".join(images_array[i].split("/")[-2:])
-        #frame["transform_matrix"] = np.linalg.inv(poses[i].detach().cpu().numpy()).tolist()
-        frame["transform_matrix"] = np.dot(poses[i].detach().cpu().numpy(),OPENGL).tolist()
+        frame["transform_matrix"] = poses[i].detach().cpu().numpy()
+        frame["transform_matrix"] = np.linalg.inv(frame["transform_matrix"])
+        frame["transform_matrix"] = OPENGL @ frame["transform_matrix"]
+        frame["transform_matrix"] = np.linalg.inv(frame["transform_matrix"])
+        frame["transform_matrix"] = frame["transform_matrix"].tolist()
         frame["mask_path"] = "/".join(masks_array[i].split("/")[-2:])
         transforms["frames"].append(frame)
         
@@ -152,7 +156,8 @@ for start_frame_index in range(INIT_FRAMES, TOTAL_FRAMES, NEW_FRAMES):
     images_array = []
     masks_array = []
 
-    preset_focal = [transforms["fl_x"]/RESCALE_FACTOR for _ in range(PREVIOUS_FRAMES+NEW_FRAMES)]
+    if USE_COMMON_INTRINSICS: preset_focal = [transforms["fl_x"]/RESCALE_FACTOR for _ in range(PREVIOUS_FRAMES+NEW_FRAMES)]
+    else: preset_focal = []
     preset_pose = []
     preset_mask = [True for _ in range(PREVIOUS_FRAMES+NEW_FRAMES)]
     preset_mask[PREVIOUS_FRAMES:] = [False for _ in range(NEW_FRAMES)]
@@ -161,14 +166,17 @@ for start_frame_index in range(INIT_FRAMES, TOTAL_FRAMES, NEW_FRAMES):
         images_array.append(os.path.join(DATA_PATH,transforms["frames"][i]["file_path"]))
         masks_array.append(os.path.join(DATA_PATH,transforms["frames"][i]["mask_path"]))
         preset_pose.append(np.array(transforms["frames"][i]["transform_matrix"]))
+        preset_focal.append(np.array(transforms["frames"][i]["fl_x"]/RESCALE_FACTOR))
         print("Refering to {}...".format(transforms["frames"][i]["file_path"]))
 
     last_known_pose = preset_pose[-1]
+    last_known_focal = preset_focal[-1]
 
     for i in range(start_frame_index, start_frame_index + NEW_FRAMES):
         images_array.append(os.path.join(DATA_PATH,"masked_images/{}{}".format(i,IMG_FILE_EXTENSION)))
         masks_array.append(os.path.join(DATA_PATH,"masks/{}{}".format(i,MASK_FILE_EXTENSION)))
         preset_pose.append(last_known_pose)
+        preset_focal.append(last_known_focal)
         print("Estimating for {}...".format(os.path.join(DATA_PATH,"masked_images/{}{}".format(i,IMG_FILE_EXTENSION))))
 
     images = load_images(images_array, size=512, verbose=True)
@@ -184,7 +192,8 @@ for start_frame_index in range(INIT_FRAMES, TOTAL_FRAMES, NEW_FRAMES):
                             weight_rot = NEW_WEIGHT_ROT ,
                             weight_trans_smoothness = NEW_WEIGHT_TRANS_SMOOTHNESS,
                             weight_rot_smoothness = NEW_WEIGHT_ROT_SMOOTHNESS)
-    scene.preset_focal(preset_focal, [True for _ in range(PREVIOUS_FRAMES+NEW_FRAMES)])
+    if USE_COMMON_INTRINSICS : scene.preset_focal(preset_focal, [True for _ in range(PREVIOUS_FRAMES+NEW_FRAMES)])
+    else: scene.preset_focal(preset_focal, preset_mask)
     scene.preset_pose(preset_pose, preset_mask)
     loss = scene.compute_global_alignment(init="mst", niter=niter, schedule=schedule, lr=lr)
     
@@ -199,8 +208,11 @@ for start_frame_index in range(INIT_FRAMES, TOTAL_FRAMES, NEW_FRAMES):
         if not((confidence_masks[i]==0).all()):
             frame = {}
             frame["file_path"] = "/".join(images_array[i].split("/")[-2:])
-            #frame["transform_matrix"] = np.linalg.inv(poses[i].detach().cpu().numpy()).tolist()
-            frame["transform_matrix"] = np.dot(poses[i].detach().cpu().numpy(),OPENGL).tolist()
+            frame["transform_matrix"] = poses[i].detach().cpu().numpy()
+            frame["transform_matrix"] = np.linalg.inv(frame["transform_matrix"])
+            frame["transform_matrix"] = OPENGL @ frame["transform_matrix"]
+            frame["transform_matrix"] = np.linalg.inv(frame["transform_matrix"])
+            frame["transform_matrix"] = frame["transform_matrix"].tolist()
             frame["mask_path"] = "/".join(masks_array[i].split("/")[-2:])
             transforms["frames"].append(frame)
             
